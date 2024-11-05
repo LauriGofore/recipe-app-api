c56e9bf8f8b0bb8acef4c6d8e6f6a9639a51ef1f
Lauri Saarela <lauri.saarela@gofore.com>
2024-11-05 13:43:02 +0200

    ..

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """TEst the calc module."""
 
     def test_add_numbers(self):
         """Test that two numbers are added together."""
         self.assertEqual(calc.add(3, 8), 11)
-
 
     def test_subtract_numbers(self):
         """Test that values are subtracted and returned."""
